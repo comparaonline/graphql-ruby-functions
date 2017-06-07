8fa197887a4af430ab0930399be55e25a70de4c6
Joaquín Moreira <jmoreiras@gmail.com>
2017-06-07 11:38:25 -0400

    Throw exceptions on border cases

--- lib/graphql/functions/base.rb
+++ lib/graphql/functions/base.rb
@@ -1,15 +1,32 @@
 require 'graphql'
+require 'active_record'
 
 module GraphQL
   module Functions
     class Base < ::GraphQL::Function
       class << self
         def create
+          fail_on_model_not_set unless @model
           self.new(@model)
         end
 
         def model(model)
+          fail_on_wrong_class_model unless model < ActiveRecord::Base
           @model = model
+        end
+
+        private
+
+        def fail_on_model_not_set
+          fail ArgumentError.new(
+            %q('model' not set. Forgot to add 'model ::ModelClass' ?)
+          )
+        end
+
+        def fail_on_wrong_class_model
+          fail ArgumentError.new(
+            %q('model' superclass mismatch. It must be 'ActiveRecord::Base')
+          )
         end
       end
 
